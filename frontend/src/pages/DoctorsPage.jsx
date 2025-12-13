f98a4409dbd3188d621f4c868a8c6b59fd719dfa
Akil-S-Khan <khanakil794@gmail.com>
2025-12-13 08:56:45 +0530

    doctot page updated

--- frontend/src/pages/DoctorsPage.jsx
+++ frontend/src/pages/DoctorsPage.jsx
@@ -4,7 +4,7 @@ import Layout from "../components/common/Layout";
 const DoctorsPage = () => {
   return (
     <Layout>
-      <h3>DoctorsPage</h3>
+      <h3>New DoctorsPage</h3>
     </Layout>
   );
 };
